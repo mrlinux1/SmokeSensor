f64a73aa255efc1cef10ddc2599e5166ee1cce1c
Thomas Gebert <mrlinux1@users.noreply.github.com>
2021-05-18 20:39:37 +0200

    Delete download.py

--- download.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-
-from tf.keras.applications import vgg16
-
-vgg_conv = vgg16.VGG16(weights='imagenet',
-                  include_top=False,
-                  input_shape=(224, 224, 3))
